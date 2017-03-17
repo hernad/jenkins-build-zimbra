c4d4f36d3f2f506dcfe193dfe028c2ae11f43d0a
hernad <hernad@bring.out.ba>
2017-03-17 21:30:23 +0100

    git pull for sure

--- vagrant.sh
+++ vagrant.sh
@@ -23,6 +23,11 @@ echo zimbra foss monolith tree \(8.7.1\)
 cd /data/build
 [ -d my-zimbra ] || git clone https://github.com/hernad/my-zimbra.git
 cd my-zimbra
+
+[ -d my-zimbra ] || exit 1
+
+sudo rm -rf *
+sudo chown vagrant -R .git
 git checkout -f
 git pull
 
