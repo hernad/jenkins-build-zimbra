d3bf0c84a59b3c2a50bff074366f6339eebd6f7e
hernad <hernad@bring.out.ba>
2017-06-03 01:23:47 +0200

    cleanup hdd vdi

--- prepare_build.sh
+++ prepare_build.sh
@@ -2,6 +2,28 @@
 
 VDI=data_zimbra-build-2.vdi
  
+WORKSPACE=zimbra-build-2
+
+if [ ! -f $VDI ] ; then
+
+  VM=`VBoxManage list vms | grep ^\"${WORKSPACE}_default_ | tail -1 | awk '{print $2}'`
+  if [ -n "$VM" ] ; then
+    echo "erasing old VM $VM"
+    VBoxManage unregistervm $VM --delete
+  else
+    echo "no old VM $WORKSPACE"
+  fi
+
+  HDD=`VBoxManage list hdds -l | grep "Location.*workspace/${WORKSPACE}/$VDI" -B7 | grep "^UUID:" | awk '{print $2}'`
+  if [ -n "$HDD" ] ; then
+     echo "erasing old HDD"
+     VBoxManage closemedium $HDD --delete
+  else
+     echo "no old HDD ${WORKSPACE}/$VDI"
+  fi
+
+fi
+
 if [ ! -f $VDI ] ;  then
   /opt/VirtualBox/VBoxManage clonemedium ~/base_$VDI $VDI
 else
